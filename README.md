486e8169fc6a7eb080f54736fecfc688e24399a0
Jon Hester <jon.d.hester@gmail.com>
2015-03-07 18:55:13 -0500

    refactor into juliandate object

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # julian-date
+
+## Create a julian date from the current time
+```js
+var JulianDate = require('julian-date');
+```
--- test/test.js
+++ test/test.js
@@ -3,42 +3,37 @@
 var assert = require("assert"); // node.js core module
 var Julian = require('../lib/julian.js');  // our module
 
-describe('Date Utilities', function(){
-
-  describe('convert gregorian to julian', function(){
-
-    it('should return correct julian date for unix epoch', function(){
-      var julianDate = Julian.toJulian(new Date(0));
-      assert.equal(2440587.5, julianDate);
-    });
-
-    it('should return correct julian date for 2015-03-07 UTC', function() {
-      var julianDate = Julian.toJulian(new Date('2015-03-07UTC'));
-      assert.equal(2457088.5, julianDate);
-    });
-
+describe('create new julian date', function() {
+  it('should create julian date with no param', function() {
+    var j = new Julian();
+    assert(j.getDate());
+    assert(j.julian());
+    assert(j.julianDays());
   });
 
-  describe('convert julian to gregorian', function() {
-    it('should return correct julian date for unix epoch', function() {
-      var julianDate = Julian.toJulian(new Date(0));
-      assert.equal(new Date(0).getTime(),Julian.fromJulian(julianDate).getTime());
-    });
-
-    it('should return correct gregorian date for 2015-03-07 UTC', function() {
-      var julianDate = Julian.toJulian(new Date('2015-03-07UTC'));
-      assert.equal(new Date('2015-03-07UTC').getTime(),Julian.fromJulian(julianDate).getTime());
-    });
+  it('should create julian date from julian date', function() {
+    var j = new Julian().julian(2440587.5);
+    assert.equal(new Date(0).valueOf(), j.getDate().valueOf());
   });
 
-  describe('convert js date to julian days', function() {
-    it('should return correct julian days for unix epoch', function() {
-      assert.equal(-10957.5,Julian.toDays(new Date(0)));
-    });
 
-    it('should return correct julian days for unix epoch', function() {
-      assert.equal(5543.5,Julian.toDays(new Date('2015-03-07UTC')));
-    });
+  it('should create julian date from julian days', function() {
+    var j = new Julian().julianDays(5543.5);
+    assert.equal(new Date('2015-03-07UTC').valueOf(),j.getDate().valueOf());
   });
 
+  it('should return correct julian date from 2015-03-07 UTC', function() {
+    var j = new Julian('2015-03-07UTC');
+    assert.equal(2457088.5,j.julian());
+  });
+
+  it('should return correct julian days from unix epoch', function() {
+    var j = new Julian(new Date(0));
+    assert.equal(-10957.5,j.julianDays());
+  });
+
+  it('should return correct julian days from 2015-03-07 UTC', function() {
+    var j = new Julian('2015-03-07UTC');
+    assert.equal(5543.5,j.julianDays());
+  });
 });
